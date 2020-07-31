0f22fa701ab85f0a51ebb209097c6e872b57b3b3
jeremiaaxel <jeremiaaxelbachtera@gmail.com>
2020-07-31 22:00:53 +0700

    update navbar & footer css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -16,52 +16,52 @@ class Header extends React.Component {
         return (
             <div class="navbar"> 
                 <ul class="navbar-left">
-                <li class="nav-item">
-                    <NavLink
-                        to="/"
-                        className="nav-link"
-                    >
-                      Habit Rabbit
-                    </NavLink>
-                </li>
-                </ul>
+                  <li class="nav-item">
+                      <NavLink
+                          to="/"
+                          className="nav-link"
+                      >
+                        Habit Rabbit
+                      </NavLink>
+                  </li>
+                  </ul>
                 <ul class="navbar-right">
-                <li class="nav-item">
-                    <NavLink
-                        to="/home"
-                        className="nav-link"
-                        activeStyle={{ fontWeight: 'bold' }}
-                    >
-                      Home
-                    </NavLink>
-                </li>
-                <li class="nav-item">
-                    <NavLink
-                        to="/about"
-                        className="nav-link"
-                        activeStyle={{ fontWeight: 'bold' }}
-                    >
-                      About
-                    </NavLink>
-                </li>
-                <li class="nav-item">
-                    <NavLink
-                        to="/login"
-                        className="nav-link"
-                        activeStyle={{ fontWeight: 'bold' }}
-                    >
-                      Login
-                    </NavLink>
-                </li>
-                <li class="nav-item">
-                    <NavLink
-                        to="/signup"
-                        className="nav-link"
-                        activeStyle={{ fontWeight: 'bold' }}
-                    >
-                      Signup
-                    </NavLink>
-                </li>
+                  <li class="nav-item">
+                      <NavLink
+                          to="/home"
+                          className="nav-link"
+                          activeStyle={{ fontWeight: 'bold' }}
+                      >
+                        Home
+                      </NavLink>
+                  </li>
+                  <li class="nav-item">
+                      <NavLink
+                          to="/about"
+                          className="nav-link"
+                          activeStyle={{ fontWeight: 'bold' }}
+                      >
+                        About
+                      </NavLink>
+                  </li>
+                  <li class="nav-item">
+                      <NavLink
+                          to="/login"
+                          className="nav-link"
+                          activeStyle={{ fontWeight: 'bold' }}
+                      >
+                        Login
+                      </NavLink>
+                  </li>
+                  <li class="nav-item">
+                      <NavLink
+                          to="/signup"
+                          className="nav-link"
+                          activeStyle={{ fontWeight: 'bold' }}
+                      >
+                        Signup
+                      </NavLink>
+                  </li>
                 </ul>
             </div>  
     );
